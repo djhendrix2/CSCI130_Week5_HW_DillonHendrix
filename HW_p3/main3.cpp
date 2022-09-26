b34111ee912810f471fcb68626eaf7e048722955
Dillon Hendrix <111817074+djhendrix2@users.noreply.github.com>
2022-09-26 04:42:38 +0000

    Working wind speed added to the table for each temp value 4 outputs

--- HW_p3/main3.cpp
+++ HW_p3/main3.cpp
@@ -17,23 +17,46 @@ int main() {
   int WS;  // Wind speed
 
   // Convert Fehrenheit Table with step variables
-  const int Max_Celcius = 10;
-  const int Min_Celsius = 2;
-  const int Step_Celsius = 4;
+  //const int Max_Celcius = 10;
+  //const int Min_Celsius = 2;
+  //const int Step_Celsius = 4;
+
+  // Wind Speed input variables
+  const int Max_WS = 11;
+  const int Min_WS = 5;
+  const int Step_WS = 2;
 
   // Table of values setup
-  cout << "Degrees    Degrees\n"
-       << "Celsius    Fahrenheit\n"
-       << "-------    ----------\n";
+  cout << "Degrees    Degrees       Wind Speed\n"
+       << "Celsius    Fahrenheit      (km/hr) \n"
+       << "-------    ----------    ----------\n";
 
   // Set output formats for the float points
   cout << setiosflags(ios::showpoint) << setprecision(2);
 
-  while(C <= Max_Celcius) {
+  /*while (C <= Max_Celcius) {
     F = (9.0/5.0) * C + 32.0;
     cout << setw(4) << C << fixed << setw(13) << F << endl;
     C = C + Step_Celsius;
+
+    while(WS <= Max_WS){
+      cout << setw(4) << WS << endl;
+    }
   }
-  return 0;
+  return 0; */
+
+  for(C = 2; C <= 10; C = C + 4){
+    F = (9.0/5.0) * C + 32.0;
+
+    for(WS = 5; WS <= 11; WS = WS + 2){
+      //cout << setw(4) << C << endl;
+
+      
+    cout << setw(4) << C << fixed << setw(13) << F << setw(13) << WS << endl;
+    }
+
+  }
+  
+    
 
 }
