6df777dc25eb24521531668f9d303a7206d90ebd
Dillon Hendrix <111817074+djhendrix2@users.noreply.github.com>
2022-09-26 02:13:30 +0000

    Working Celsius to Fahrenheit conversion table with proper increments

--- HW_p3/main3.cpp
+++ HW_p3/main3.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
   Programmer: Dillon Hendrix
-  Discription:
- Date: 09/20/2022
+  Discription: Use a wind chill display formula to determine wind chill indexes for both the U.S. and Canada
+ Date: 09/24/2022
   *******************************************************************/
 
 #include <fstream>
@@ -10,4 +10,30 @@
 #include <string>
 using namespace std;
 
-int main() {}
+int main() {
+  // initialize variables
+  int C; // Celsius
+  double F;  // Fahrenheit
+  int WS;  // Wind speed
+
+  // Convert Fehrenheit Table with step variables
+  const int Max_Celcius = 10;
+  const int Min_Celsius = 2;
+  const int Step_Celsius = 4;
+
+  // Table of values setup
+  cout << "Degrees    Degrees\n"
+       << "Celsius    Fahrenheit\n"
+       << "-------    ----------\n";
+
+  // Set output formats for the float points
+  cout << setiosflags(ios::showpoint) << setprecision(2);
+
+  while(C <= Max_Celcius) {
+    F = (9.0/5.0) * C + 32.0;
+    cout << setw(4) << C << fixed << setw(13) << F << endl;
+    C = C + Step_Celsius;
+  }
+  return 0;
+
+}
